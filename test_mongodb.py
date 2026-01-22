3e5f14b2fb293801227f80419952ecd587b9f2cf
madhavkumaryadav <ymadhav081@gmail.com>
2026-01-22 11:57:58 +0530

    ETL Pipeline Completed

---
+++ test_mongodb.py
@@ -0,0 +1,17 @@
+from collections.abc import MutableMapping
+from collections import namedtuple
+from pymongo import MongoClient # type: ignore
+
+
+uri="mongodb+srv://root:MADhav44@collage.qvvopgb.mongodb.net/?appName=Collage"
+
+
+## Create a new client and connect to the server 
+client=MongoClient(uri)
+
+## Send a ping to confirm a successful connection 
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
